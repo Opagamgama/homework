65f32d84d4898f7665a062b31e5f5fbeaf347ee6
Opagamgama <145692020+Opagamgama@users.noreply.github.com>
2023-10-04 12:37:56 -0700

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,11 @@
+mama=50
+print(mama+20)    # 20 lis mere mamas asaki
+
+deda=50
+print(deda+20)   #20 wlis mere dedas asaki
+
+me=15
+print(me+20)     #20 wlis mere chemi asaki
+
+dzma=16
+print(dzma+20)   #20 wlis mere dzmis asaki
