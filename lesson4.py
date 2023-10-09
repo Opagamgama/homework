102d0def9a22044d9efe1009dffe9242414b20b7
Opagamgama <145692020+Opagamgama@users.noreply.github.com>
2023-10-09 03:39:30 -0700

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,7 @@
+user_money= float(input("sheiyvanet tqveni xelfasi"))
+if user_money>10000:
+    print("goashi swavlobdit?")
+elif user_money >1000 and user_money <10000:
+    print("you mid")
+else:
+    print("shemosuliyavi goashi matricelo")
